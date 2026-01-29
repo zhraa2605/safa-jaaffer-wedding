18c514181e80c0721044db9597d57d57f1e965ec
zhraa2605 <zhraa.e.l.2605@gmail.com>
2026-01-29 19:22:52 +0300

    Adjust margin for Quran verse image in wedding invitation

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
                     <Image
                       src="/quran-verse.png"
                       alt="آية قرآنية"
-                      className="mx-auto  -mt-4 object-contain"
+                      className="mx-auto mt-0 object-contain"
                       onError={() => setVerseImageError(true)}
                       width={1000}
                       height={50}
